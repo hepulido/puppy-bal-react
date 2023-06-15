1a9fb28942d91b421a9523cd4a904a13032b2809
hector <hpulido1021@gmail.com>
2023-06-15 19:21:48 -0400

    render puppy

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { puppyList } from "./data.js";
+import "./App.css";
 
 function App() {
-  // const [count, setCount] = useState(0)
+
+  const [puppies, setPuppies] = useState(puppyList);
+
+  console.log("puppyList: ", puppyList);
 
   return (
-    <>
-     
-    </>
-  )
+    <div className="App">
+      {
+        puppies.map((puppy) => {
+             return <p key={puppy.id}>{puppy.name}</p>;
+           })
+       }
+    </div>
+  );
 }
 
-export default App
+export default App;
