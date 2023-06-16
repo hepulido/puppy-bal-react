742d21dbf73330915ccbb1cf85319163499e3f3f
hector <hpulido1021@gmail.com>
2023-06-15 20:52:57 -0400

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,35 @@ import { useState } from "react";
 import { puppyList } from "./data.js";
 import "./App.css";
 
-function App() {
-
-  const [puppies, setPuppies] = useState(puppyList);
-
-  console.log("puppyList: ", puppyList);
-
-  return (
-    <div className="App">
-      {
-        puppies.map((puppy) => {
-             return <p key={puppy.id}>{puppy.name}</p>;
-           })
-       }
-    </div>
-  );
-}
+  function App() {
+    const [puppies, setPuppies] = useState(puppyList);
+    const [featPupId, setFeatPupId] = useState(null);
+  
+    function handleClick() {
+     setFeatPupId(puppies.id)
+    }
+   const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+    return (
+      <div className="App">
+        {puppies.map((puppy) => {
+          return (
+            <p onClick={handleClick} key={puppy.id}>
+              {puppy.name}
+            </p>
+          );
+        })
+        }
+        {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
+      </div>
+    );
+  }
 
 export default App;
